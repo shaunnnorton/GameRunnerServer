72f72684138bd741f30fb6ac1dfd34731d11c403
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2021-03-23 10:10:33 -0700

    Changed DB

--- api_app/__init__.py
+++ api_app/__init__.py
@@ -4,7 +4,7 @@ import os
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
